98979edd3b7c4e9ae76aa13e25abd1cc48fc12ad
Patrick Denning <pdenning1993@gmail.com>
2022-03-26 16:20:35 +0000

    starting to work on adding more topics to publisher

--- publisher/publish.cpp
+++ publisher/publish.cpp
@@ -18,6 +18,12 @@ using namespace exploringRPi;
 #define QOS        1
 #define TIMEOUT    10000L
 
+#define TOPIC_TEMP      "ee513/CPUTemp"
+#define TOPIC_ACCL "ee513/Accl"
+#define TOPIC_ROLL "ee513/Roll"
+#define TOPIC_PITCH "ee513/Pitch"
+
+
 float getCPUTemperature() {         //get the CPU temperature
    int cpuTemp;                     //store as an int
    fstream fs;
@@ -27,9 +33,14 @@ float getCPUTemperature() {         //get the CPU temperature
    return (((float)cpuTemp)/1000);
 }
 
+// helper function to generate a json payload from topic string and data string
+// data must first be converted to string form
+void generateJson(char* str_payload, char* topic, char* data) {
+   sprintf(str_payload, "{\"d\":{\"%s\": %s }}", topic, data);
+}
 
-int main(int argc, char* argv[]) {
-   char str_payload[100];           //Set your max message size here
+int publishMessage(char* str_payload, char* topic){
+   // setup client and connect to broker
    MQTTClient client;
    MQTTClient_connectOptions opts = MQTTClient_connectOptions_initializer;
    MQTTClient_message pubmsg = MQTTClient_message_initializer;
@@ -45,15 +56,7 @@ int main(int argc, char* argv[]) {
       return -1;
    }
 
-   // Code added to interface to ADXL345
-   ADXL345 theAdxl(1, 0x53);
-   theAdxl.setResolution(ADXL345::NORMAL);
-   theAdxl.setRange(ADXL345::PLUSMINUS_4_G);
-   theAdxl.readSensorState();
-
-   sprintf(str_payload, "{\"d\":{\"CPUTemp\": %f, \"X Accel\": %d, \"Y Accel\": %d, \"Z Accel\": %d, \"Pitch\": %f, \"Roll\": %f }}",
-			 getCPUTemperature(), theAdxl.getAccelerationX(), theAdxl.getAccelerationY(), theAdxl.getAccelerationZ(),
-			theAdxl.getPitch(), theAdxl.getRoll());
+   // create and send the message, wait for completion
    pubmsg.payload = str_payload;
    pubmsg.payloadlen = strlen(str_payload);
    pubmsg.qos = QOS;
@@ -64,7 +67,63 @@ int main(int argc, char* argv[]) {
         " \non topic " << TOPIC << " for ClientID: " << CLIENTID << endl;
    rc = MQTTClient_waitForCompletion(client, token, TIMEOUT);
    cout << "Message with token " << (int)token << " delivered." << endl;
+
+   // disconnect and destoy client
    MQTTClient_disconnect(client, 10000);
    MQTTClient_destroy(&client);
    return rc;
+}
+
+
+int main(int argc, char* argv[]) {
+   char str_payload[100];           //Set your max message size here
+   // MQTTClient client;
+   // MQTTClient_connectOptions opts = MQTTClient_connectOptions_initializer;
+   // MQTTClient_message pubmsg = MQTTClient_message_initializer;
+   // MQTTClient_deliveryToken token;
+   // MQTTClient_create(&client, ADDRESS, CLIENTID, MQTTCLIENT_PERSISTENCE_NONE, NULL);
+   // opts.keepAliveInterval = 20;
+   // opts.cleansession = 1;
+   // opts.username = AUTHMETHOD;
+   // opts.password = AUTHTOKEN;
+   // int rc;
+   // if ((rc = MQTTClient_connect(client, &opts)) != MQTTCLIENT_SUCCESS) {
+   //    cout << "Failed to connect, return code " << rc << endl;
+   //    return -1;
+   // }
+
+   // Code added to interface to ADXL345
+   ADXL345 theAdxl(1, 0x53);
+   theAdxl.setResolution(ADXL345::NORMAL);
+   theAdxl.setRange(ADXL345::PLUSMINUS_4_G);
+   theAdxl.readSensorState();
+
+
+   char temp[20];
+   sprintf(temp, "%f", getCPUTemperature());
+   generateJson(str_payload, TOPIC_TEMP, temp);
+   publishMessage(str_payload, TOPIC_TEMP);
+   
+
+   // // create the payload
+   // sprintf(str_payload, "{\"d\":{\"CPUTemp\": %f, \"X Accel\": %d, \"Y Accel\": %d, \"Z Accel\": %d, \"Pitch\": %f, \"Roll\": %f }}",
+	// 		 getCPUTemperature(), theAdxl.getAccelerationX(), theAdxl.getAccelerationY(), theAdxl.getAccelerationZ(),
+	// 		theAdxl.getPitch(), theAdxl.getRoll());
+   
+   // // create and send the message, wait for completion
+   // pubmsg.payload = str_payload;
+   // pubmsg.payloadlen = strlen(str_payload);
+   // pubmsg.qos = QOS;
+   // pubmsg.retained = 0;
+   // MQTTClient_publishMessage(client, TOPIC, &pubmsg, &token);
+   // cout << "Waiting for up to " << (int)(TIMEOUT/1000) <<
+   //      " seconds for publication of " << str_payload <<
+   //      " \non topic " << TOPIC << " for ClientID: " << CLIENTID << endl;
+   // rc = MQTTClient_waitForCompletion(client, token, TIMEOUT);
+   // cout << "Message with token " << (int)token << " delivered." << endl;
+
+   // // disconnect and destoy client
+   // MQTTClient_disconnect(client, 10000);
+   // MQTTClient_destroy(&client);
+   // return rc;
 }
